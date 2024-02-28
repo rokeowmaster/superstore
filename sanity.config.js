0ddd4e35a02586629b29298fbad193ec50db51d4
rokeowmaster <rokeowmaster@gmail.com>
2024-02-28 04:54:19 -0800

    boilerplate

---
+++ sanity.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'superstore',
+
+  projectId: '63tc7q9w',
+  dataset: 'production',
+  basePath: "/studio",
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
---
+++ sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '63tc7q9w',
+    dataset: 'production'
+  }
+})
